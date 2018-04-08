c50bf831cb5c7db71472877ac1aa3dabee1cddef
Moni Monroe <maguado85@gmail.com>
2018-04-08 17:07:10 +0200

    favicon and data styles

--- js/main.js
+++ js/main.js
@@ -1,34 +1,5 @@
 'use strict';
 
-//   fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json')
-//   .then(function(response){
-//     return response.json();
-//   })
-//   .then(function(json){
-//     // Name
-//     const recipe = json.recipe.name;
-//     const nameHTML = document.querySelector('.header__title');
-//     // Ingredients
-//     const ingredients = json.recipe.ingredients;
-//     const listHTML = '';
-//     for (var i = 0; i < ingredients.length; i++) {
-//       listHTML +=
-//       '<li class="ingredients__item">
-//         <input class="item__checkbox" type="checkbox">
-//           <input class="item__quantity" id="item${i}--quantity" name="input--quantity" value="1" type="number">
-//             <div class="item__description">
-//               <h4 class="item__description--title">${ingredients[i].product}</h4>
-//               <p class="item__description--brand">${ingredients[i].brand? `${ingredients[i].brand}` : ''}</p>
-//               <p class="item__description--weight">${ingredients[i].quantity}</p>
-//             </div>
-//             <span class="item__price">${ingredients[i].price} €</span>
-//           </li>'
-//     }
-//   })
-//
-// const listIngredients = document.querySelector('.ingredients__list');
-// listIngredients.innerHTML = listHTML;
-
 fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json')
   .then(function(response){
     return response.json();
@@ -44,6 +15,7 @@ fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-seta
     for (let i = 0; i < ingredients.length; i++) {
       let item =
       `<li class="ingredients__item">
+        <div class="ingredients__data">
           <input class="item__checkbox" type="checkbox">
           <input class="item__quantity" id="item${i}--quantity" name="input--quantity" value="1" type="number">
           <div class="item__description">
@@ -51,7 +23,10 @@ fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-seta
             <p class="item__description--brand">${ingredients[i].brand? `${ingredients[i].brand}` : ''}</p>
             <p class="item__description--weight">${ingredients[i].quantity}</p>
           </div>
+        </div>
+        <div class="price__data">
           <span class="item__price">${ingredients[i].price} €</span>
+        </div>
       </li>`
 const listIngredients = document.querySelector('.ingredients__list')
       listIngredients.insertAdjacentHTML('beforeend', item);
