07dc19cce4a18ee00bc36936f81d1f363bec783f
Moni Monroe <maguado85@gmail.com>
2018-04-09 18:53:59 +0200

    trying total price

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-seta
       `<li class="ingredients__item">
         <div class="ingredients__data">
           <input class="item__checkbox" type="checkbox">
-          <input class="item__quantity" id="item${i}--quantity" name="input--quantity" value="1" type="number">
+          <input class="item__quantity" onChange='paint()' id="item${i}--quantity" name="input--quantity" value="1" type="number">
           <div class="item__description">
             <h4 class="item__description--title">${ingredients[i].product}</h4>
             <p class="item__description--brand">${ingredients[i].brand? `${ingredients[i].brand}` : ''}</p>
@@ -35,3 +35,35 @@ const listIngredients = document.querySelector('.ingredients__list')
   .catch(function(error){
     console.log('Ha sucedido un error: ' + error);
 })
+
+//Select items
+const selectAll = () => {
+let all = document.querySelectorAll("input[type=checkbox]");
+for( let i = 0; i < all.length;i++){
+  all[i].checked = true;
+  }
+}
+//Unselect items
+const unselectAll = () => {
+let all = document.querySelectorAll("input[type=checkbox]");
+for( let i = 0; i < all.length; i++){
+  all[i].checked = false;
+  }
+}
+
+const unitProduct = () => {
+  let checkbox = document.querySelectorAll("input[type=checkbox]");
+  let product = document.getElementById("item${i}--quantity").value;
+  let priceProduct = ingredients[i].price
+  let totalPriceProduct = document.querySelector('.resume__button--total');
+  for (var i = 0; i < checkbox.length; i++){
+  if (checkbox[i].checked = true) {
+     totalPriceProduct = product * priceProduct;
+   }
+  }
+}
+
+const paint = () => {
+  let paintPrice = document.getElementById("item${i}--quantity");
+  paintPrice.addEventListener("change", unitProduct);
+}
